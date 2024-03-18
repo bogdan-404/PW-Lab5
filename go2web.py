396d570ad276f5a50ffee35b5c979d0d97c6676b
bogdan-404 <bogdan.zlatovcen@gmail.com>
2024-03-18 12:33:31 +0200

    Add functionality to send http request

--- go2web.py
+++ go2web.py
@@ -3,6 +3,16 @@ import sys
 import os
 
 
+def send_request(host, port, request):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.sendall(request)
+        response = s.recv(4096)
+        while len(response) > 0:
+            print(response.decode("utf-8", "ignore"), end="")
+            response = s.recv(4096)
+
+
 def print_help():
     help_text = """Usage: go2web [OPTION]... [ARGUMENT]...
     -u <URL>         make an HTTP request to the specified URL and print the response
